99246ab6648a2f9eca7daddd53946806791d4e06
Billa <prshanthbilla009@gmail.com>
2021-07-14 12:29:13 +0530

    added lombok

--- src/main/java/com/example/demo/exception_handling/AppResponse.java
+++ src/main/java/com/example/demo/exception_handling/AppResponse.java
@@ -1,37 +1,14 @@
 package com.example.demo.exception_handling;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter@Getter
+@NoArgsConstructor@AllArgsConstructor
 public class AppResponse {
     private int status;
     private String message;
     private Object data;
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
-
-    public AppResponse(int status, String message, Object data) {
-        this.status = status;
-        this.message = message;
-        this.data = data;
-    }
 }
--- src/main/java/com/example/demo/model/EmployeeModel.java
+++ src/main/java/com/example/demo/model/EmployeeModel.java
@@ -1,48 +1,21 @@
 package com.example.demo.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class EmployeeModel {
+
     private Integer empId;
     private String empName;
     private String empMobile;
     private double empSal;
 
-    public Integer getEmpId() {
-        return empId;
-    }
-
-    public void setEmpId(Integer empId) {
-        this.empId = empId;
-    }
-
-    public String getEmpName() {
-        return empName;
-    }
-
-    public void setEmpName(String empName) {
-        this.empName = empName;
-    }
-
-    public String getEmpMobile() {
-        return empMobile;
-    }
-
-    public void setEmpMobile(String empMobile) {
-        this.empMobile = empMobile;
-    }
-
-    public double getEmpSal() {
-        return empSal;
-    }
-
-    public void setEmpSal(double empSal) {
-        this.empSal = empSal;
-    }
-
-    public EmployeeModel(Integer empId, String empName, String empMobile, double empSal) {
-        this.empId = empId;
-        this.empName = empName;
-        this.empMobile = empMobile;
-        this.empSal = empSal;
-    }
 
 }
