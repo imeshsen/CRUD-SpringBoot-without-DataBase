7591e6d4f41cf87f53dd81fa0889715aa1d6cb57
Billa <prshanthbilla009@gmail.com>
2021-07-19 16:26:47 +0530

    Updated RestClient

--- src/main/java/com/example/demo/SpringCrudWithoutDbApplication.java
+++ src/main/java/com/example/demo/SpringCrudWithoutDbApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringCrudWithoutDbApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SpringCrudWithoutDbApplication.class, args);
     }
--- src/main/java/com/example/demo/rest_client/RestClient.java
+++ src/main/java/com/example/demo/rest_client/RestClient.java
@@ -5,6 +5,7 @@ import com.example.demo.model.EmployeeModel;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,10 +16,12 @@ import java.util.List;
 @RequestMapping("/myApp")
 public class RestClient {
     private static final String GET_ALL_EMPLOYEES_API = "http://localhost:8080/employees";
+    private static final String GET_EMPLOYEE_BY_ID="http://localhost:8080/getEmployee/2";
     static RestTemplate restTemplate = new RestTemplate();
 
     public static void main(String[] args) {
         getAllEmployees();
+        getEmployeeById();
     }
     @GetMapping("/getAll")
     private static ResponseEntity<Object> getAllEmployees(){
@@ -26,9 +29,16 @@ public class RestClient {
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<Object> entity=new HttpEntity<>("parameters",headers);
         ResponseEntity<Object> result=restTemplate.exchange(GET_ALL_EMPLOYEES_API, HttpMethod.GET,entity,Object.class);
-        AppResponse response = new AppResponse(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase(),result);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
-    /*@GetMapping("get/{empId}")
-    private static ResponseEntity<Object>*/
+    @GetMapping("/get/2")
+    private static ResponseEntity<Object> getEmployeeById(){
+        HttpHeaders headers=new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<Object> entity=new HttpEntity<>("parameters",headers);
+        ResponseEntity<Object> result=restTemplate.exchange(GET_EMPLOYEE_BY_ID, HttpMethod.GET,entity,Object.class);
+        AppResponse response = new AppResponse(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase(),result);
+        return new ResponseEntity<>(response,HttpStatus.OK);
+
+    }
 }
