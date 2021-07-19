ed45137498cf4777b05b25fbcce5846d8d0104a7
Billa <prshanthbilla009@gmail.com>
2021-07-19 19:02:40 +0530

    Updated RestClientControllerClass

--- src/main/java/com/example/demo/SpringCrudWithoutDbApplication.java
+++ src/main/java/com/example/demo/SpringCrudWithoutDbApplication.java
@@ -2,10 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringCrudWithoutDbApplication {
-
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCrudWithoutDbApplication.class, args);
--- src/main/java/com/example/demo/model/EmployeeModel.java
+++ src/main/java/com/example/demo/model/EmployeeModel.java
@@ -1,10 +1,7 @@
 package com.example.demo.model;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
